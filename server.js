9330b02c83e7683376a61611e711e72e66ee3bf6
Kevin20-blip <kevinkigen081@gmail.com>
2026-04-10 01:26:59 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
+const express = require("express");
+const cors = require("cors");
 
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("🚀 SmartInvest API is running...");
+});
+
+// IMPORTANT: use Render port
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
