16891e65d035478d700aebc61d8dd35f4388510b
Kevin20-blip <kevinkigen081@gmail.com>
2026-04-10 09:38:16 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,15 +4,33 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Middleware to read JSON
+app.use(express.json());
+
 // Serve frontend
 app.use(express.static(path.join(__dirname, "public")));
 
-// API with dynamic data
+// TEMP STORAGE (server memory)
+let investmentData = {
+  portfolio: 0,
+  profit: 0
+};
+
+// GET data
 app.get("/api", (req, res) => {
-  res.json({
-    portfolio: 12500,
-    profit: 1800
-  });
+  res.json(investmentData);
+});
+
+// POST (save data)
+app.post("/api", (req, res) => {
+  const { portfolio, profit } = req.body;
+
+  investmentData = {
+    portfolio,
+    profit
+  };
+
+  res.json({ message: "Saved successfully ✅" });
 });
 
 // Start server
